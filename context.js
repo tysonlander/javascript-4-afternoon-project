c92e66a3297ec467a180146ec1aff7b2d1abdbb8
tysonlander <tysonlander@gmail.com>
2019-04-26 17:16:36 -0600

    initial commit

--- context.js
+++ context.js
@@ -17,7 +17,14 @@
   email --> which is a string
   getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 */
-  
+
+let user = {
+  username: "",
+  email: '',
+  getUsername: function() {
+    return this.username
+  }
+}
 
 //Code Here
 
